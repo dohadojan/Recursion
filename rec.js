772cd86e8cd42442302894d7c6cdc39a49edce83
Your Name <foo@example.com>
2019-07-11 15:44:03 +0300

    solve ex12,11

--- rec.js
+++ rec.js
@@ -8,13 +8,13 @@
 // Ex: sum(5); => 5 + 4 + 3 + 2 + 1 + 0 => 15
 
 function sum(n) {
-    if (n == 0) {
-  
-      return n
-    }
-    return n + sum(n - 1)
+  if (n == 0) {
+
+    return n
   }
-  console.log(sum(4))
+  return n + sum(n - 1)
+}
+console.log(sum(4))
 
 // 2
 // Create a function called factorial 
@@ -52,15 +52,15 @@ n:      0 1 2 3 4 5  6  7
 */
 function fibon(num) {
 
-    if(num < 0)
-        return 0;
-    if(num === 0)
-        return 1;
-    return fibon(num - 1) + fibon(num - 2);
-  }
-  fibon(5)
-  
-  /*
+  if (num < 0)
+    return 0;
+  if (num === 0)
+    return 1;
+  return fibon(num - 1) + fibon(num - 2);
+}
+fibon(5)
+
+/*
 
 
 
@@ -75,18 +75,18 @@ Ex: sum2(4, 5); => 4 + 5 => 9
 Ex: sum2(3, 6); => 3 + 4 + 5 + 6 => 18
 */
 
-function sum2(x,y){
+function sum2(x, y) {
 
-    if(x===y){
-  
-   return y;
-  
+  if (x === y) {
+
+    return y;
+
   }
-    return (x+sum2((x+1),y))
-  }
-  
-  
-  /*
+  return (x + sum2((x + 1), y))
+}
+
+
+/*
 
 
 // /*
@@ -111,7 +111,7 @@ function sum2(x,y){
 // }
 
 // repeatStr2("do","fff")
-  
+
 
 
 
@@ -126,15 +126,15 @@ function sum2(x,y){
 // Ex: multiOf(6,2,4); => 96
 // 
 
-function multiOf(n1,n2,n3){
-    if(n3===0){
-      return n1  ;
-    }
+function multiOf(n1, n2, n3) {
+  if (n3 === 0) {
+    return n1;
+  }
 
-      return(n2*multiOf(n1,n2,n3-1))
+  return (n2 * multiOf(n1, n2, n3 - 1))
 
 }
-console.log( multiOf(5,4,10))
+console.log(multiOf(5, 4, 10))
 
 
 // /*
@@ -147,58 +147,58 @@ console.log( multiOf(5,4,10))
 // */
 
 
-function  muti2(x,y){
+function muti2(x, y) {
 
-    if(x===y){
-  
-   return y;
-  
-  }
-    return (x*muti2((x+1),y))
+  if (x === y) {
 
-
-
-
+    return y;
 
   }
+  return (x * muti2((x + 1), y))
 
-  console.log(muti2(4,5))
+
+
+
+
+}
+
+console.log(muti2(4, 5))
 
 //9
-function numberBetweenUs(num1 , num2){
-    if (num1===num2-1||num1===num2-2 ){
-   
-      return num2-1;
-   
-      }
-   
-   
-    return (num1+1)+" "+(numberBetweenUs(  (num1+1) , num2  )  );
-   }
-   console.log(numberBetweenUs(2,8))
+function numberBetweenUs(num1, num2) {
+  if (num1 === num2 - 1 || num1 === num2 - 2) {
+
+    return num2 - 1;
+
+  }
+
+
+  return (num1 + 1) + " " + (numberBetweenUs((num1 + 1), num2));
+}
+console.log(numberBetweenUs(2, 8))
 
 
 
 
-  //  10
-  //  Write a function called countDown
-  //  that takes one parameter
-  //  and return a string represent the count down from the number
-  //  that insert to 0
-  //  instead of 0 need to ahve "done"
-  //  between each number a comma and space
-  //  ** you need to use recursion
-  //  ** Important: You should dint have extra space at the end or comma
-  //  countDown(5)
-  //  => "5, 4, 3, 2, 1, done"
-  //  countDown(2)
-  //  => "2, 1, done"
-  //  countDown(7)
-  //  => "7, 6, 5, 4, 3, 2, 1, done"
-  //  */
+//  10
+//  Write a function called countDown
+//  that takes one parameter
+//  and return a string represent the count down from the number
+//  that insert to 0
+//  instead of 0 need to ahve "done"
+//  between each number a comma and space
+//  ** you need to use recursion
+//  ** Important: You should dint have extra space at the end or comma
+//  countDown(5)
+//  => "5, 4, 3, 2, 1, done"
+//  countDown(2)
+//  => "2, 1, done"
+//  countDown(7)
+//  => "7, 6, 5, 4, 3, 2, 1, done"
+//  */
 
 
-  /*
+/*
 11
 Write a function called multiplication2
 that takes two parameters
@@ -210,103 +210,111 @@ multiplication2(7,6) =>  42
 */
 
 
-
-function mod2(num1,num2){
-  if(num1<num2){
-   return num1;
+function multiplication2(num1, num2) {
+  if (num2 === 0) {
+    return 0;
   }
- 
-  return  mod2(num1-num2,num2 )
- }
- console.log(mod2(8,5))
-   
-   /*
-   11
-   Write a function called multiplication2
-   that takes two parameters
-   and return the multiplication of them
-   you need to use only sum
-   multiplication2(5,4) => 20
-   multiplication2(2,8) => 16
-   multiplication2(7,6) =>  42
-   */
+  return (num1 + multiplication2(num1, num2 - 1));
 
-   
-   
-   /*
-   11
-   Write a function called mod2
-   that takes two parameters
-   and return the module of them
-   without using %
-   mod2(5,4) => 1
-   mod2(2,8) => 2
-   mod2(7,4) => 3
-   */
-   
-   
-   /*
-   12
-   Write a function called mirror
-   that takes one parameter a string
-   and return the reversed string
-   **hint: "str"[0] => "s"
-   **hint: "car"[1] => "a"
-   ** Important: You should dint have extra space at the end
-   mirror("school") => "loohcs"
-   mirror("car") => "rac"
-   mirror("maDrasa") => "asarDam"
-   */
-   
-   /*
-   13
-   Write a function called mirrorCaseAlso
-   that takes one parameter a string
-   and return the reversed string order
-   and the lower case to upper case
-   and the upper case to lower case
-   **hint: seacrh in MDN how "a" => "A" and "A" => "a"
-   ** Important: You should dint have extra space at the end
-   mirrorCaseAlso("sChOol") => "LOoHcS"
-   mirrorCaseAlso("THOR") => "roht"
-   mirrorCaseAlso("BaBa") => "AbAb"
-   */
-   
-   /*
-   14
-   Write a function called repeatChar
-   that takes two parameters
-   and return the times that this char
-   without case sensetive
-   repeat inside this string
-   **hint: "str"[0] => "s"
-   **hint: "car"[1] => "a"
-   repeatChar("schOol","o") => 2
-   repeatChar("school","a") => 0
-   repeatChar("School","s") => 1
-   try more case by yourself
-   */
-   
-   /*
-   15
-   Write a function called repeatChar2
-   that takes two parameters
-   and return the times that this char
-   wit case sensetive
-   repeat inside this string
-   **hint: "str"[0] => "s"
-   **hint: "car"[1] => "a"
-   **hint: seacrh in MDN how "a" => "A" and "A" => "a"
-   repeatChar2("schOol","o") => 1
-   repeatChar2("school","a") => 0
-   repeatChar2("School","S") => 1
-   try more case by yourself
-   */
+}
+
+console.log(multiplication2(5, 4))
 
 
 
 
 
 
+/*
+11
+Write a function called mod2
+that takes two parameters
+and return the module of them
+without using %
+mod2(5,4) => 1
+mod2(2,8) => 2
+mod2(7,4) => 3
+*/
+function mod2(num1, num2) {
+  if (num1 < num2) {
+    return num1;
+  }
 
-   
+  return mod2(num1 - num2, num2)
+}
+console.log(mod2(8, 5))
+
+/*
+12
+Write a function called mirror
+that takes one parameter a string
+and return the reversed string
+**hint: "str"[0] => "s"
+**hint: "car"[1] => "a"
+** Important: You should dint have extra space at the end
+mirror("school") => "loohcs"
+mirror("car") => "rac"
+mirror("maDrasa") => "asarDam"
+*/
+function reverseString(str) {
+
+  return str.split("").reduce((revString, char) => char + revString, "");
+
+}
+
+console.log(reverseString("school"))
+
+/*
+13
+Write a function called mirrorCaseAlso
+that takes one parameter a string
+and return the reversed string order
+and the lower case to upper case
+and the upper case to lower case
+**hint: seacrh in MDN how "a" => "A" and "A" => "a"
+** Important: You should dint have extra space at the end
+mirrorCaseAlso("sChOol") => "LOoHcS"
+mirrorCaseAlso("THOR") => "roht"
+mirrorCaseAlso("BaBa") => "AbAb"
+*/
+
+
+
+
+/*
+14
+Write a function called repeatChar
+that takes two parameters
+and return the times that this char
+without case sensetive
+repeat inside this string
+**hint: "str"[0] => "s"
+**hint: "car"[1] => "a"
+repeatChar("schOol","o") => 2
+repeatChar("school","a") => 0
+repeatChar("School","s") => 1
+try more case by yourself
+*/
+
+/*
+15
+Write a function called repeatChar2
+that takes two parameters
+and return the times that this char
+wit case sensetive
+repeat inside this string
+**hint: "str"[0] => "s"
+**hint: "car"[1] => "a"
+**hint: seacrh in MDN how "a" => "A" and "A" => "a"
+repeatChar2("schOol","o") => 1
+repeatChar2("school","a") => 0
+repeatChar2("School","S") => 1
+try more case by yourself
+*/
+
+
+
+
+
+
+
